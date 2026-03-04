7c8d599cff44f4b363f2a85ff22070f6eb35d3b0
Poky16 <bodoloczkybence60@gmail.com>
2026-03-04 17:36:09 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -19,7 +19,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
         console.log('Slash parancsok frissítése...');
 
         await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            Routes.applicationCommands(process.env.CLIENT_ID),
             { body: commands }
         );
 
@@ -27,4 +27,5 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
     } catch (error) {
         console.error(error);
     }
+
 })();
